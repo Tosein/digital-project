6a2071f74517abea75aeae00b17aba6f64614e15
Tosein <olayiwolaoluwatosin7@gmail.com>
2024-03-10 12:08:27 +0100

    Designed the front page of the project using bootstrap css

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,20 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use app\Models\User;
 
 class HomeController extends Controller
 {
+    function index(){
+        return view('home.userpage');
+    }
+
     function redirect()
     {
         $usertype = Auth::user()->usertype;
-
-        if ($usertype == '1') {
-            return view('admin.home');
-        } else {
-            return view('dashboard');
+        if($usertype=='1'){
+            return view ('admin.home');
+        }
+        else {
+            return view ('dashboard');
         }
     }
 }
